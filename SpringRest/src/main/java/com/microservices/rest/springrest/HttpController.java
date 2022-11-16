11b93d1fa465c0efa9e64421944004595e73eb40
Leonardo <swotino@hotmail.it>
2022-11-16 16:37:13 +0100

    Java Spring Rest

---
+++ SpringRest/src/main/java/com/microservices/rest/springrest/HttpController.java
@@ -0,0 +1,15 @@
+package com.microservices.rest.springrest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/")
+public class HttpController {
+
+    @GetMapping("hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
--- sources/simple_project.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-parameters = sys.argv
-if len(parameters) > 1:
-    name = parameters[1]
-else:
-    name = "Leonardo"
-print(f'Hello {name}!')
