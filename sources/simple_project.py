e6a4aba1c0dc9abd21ce8ded582b9dbb9c3004b1
Leonardo <swotino@hotmail.it>
2022-11-16 12:57:49 +0100

    Final version

--- sources/simple_project.py
+++ sources/simple_project.py
@@ -1,11 +1,9 @@
 import sys
 
-print('First Python app')
+
 parameters = sys.argv
 if len(parameters) > 1:
     name = parameters[1]
 else:
     name = "Giorgio"
 print(f'Hello {name}!')
-
-print('End project')
