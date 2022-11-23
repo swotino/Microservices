1881494df716ada57c6ff58013763f9b3a093970
Leonardo <swotino@hotmail.it>
2022-11-23 14:44:09 +0100

    Add python flask

--- Python/app.py
+++ Python/app.py
@@ -1,4 +1,7 @@
 import sys
+from flask import Flask, request
+
+app = Flask(__name__)
 
 def add(a, b):
     return a + b
@@ -12,18 +15,44 @@ def mul(a, b):
 def div(a, b):
     return a / b
 
-args = sys.argv
-if len(args) == 4:
-    cmd = args[1]
-    if 'add' in cmd:
-        print(add(float(args[2]), float(args[3])))
-    elif 'sub' in cmd:
-        print(sub(float(args[2]), float(args[3])))
-    elif 'mul' in cmd:
-        print(mul(float(args[2]), float(args[3])))
-    elif 'div' in cmd:
-        print(div(float(args[2]), float(args[3])))
+
+@app.route("/calculate")
+def calculate():
+    args = request.args
+    if len(args) == 3:
+        cmd = args[0]
+        if 'add' in cmd:
+            return add(float(args[1]), float(args[2]))
+        elif 'sub' in cmd:
+            return sub(float(args[1]), float(args[2]))
+        elif 'mul' in cmd:
+            return mul(float(args[1]), float(args[2]))
+        elif 'div' in cmd:
+            return div(float(args[1]), float(args[2]))
+        else:
+            return "Unknown command"
     else:
-        print("Unknown command")
-else:
-    print("Wrong parameters")
+        return "Wrong command"
+
+try:
+    if __name__ == '__main__':
+        app.run("0.0.0.0", 9090)
+except Exception as error:
+    print(f"ERROR in webserver: {error}")
+
+
+#args = sys.argv
+#if len(args) == 4:
+#    cmd = args[1]
+#    if 'add' in cmd:
+#        print(add(float(args[2]), float(args[3])))
+#    elif 'sub' in cmd:
+#        print(sub(float(args[2]), float(args[3])))
+#    elif 'mul' in cmd:
+#        print(mul(float(args[2]), float(args[3])))
+#    elif 'div' in cmd:
+#        print(div(float(args[2]), float(args[3])))
+#    else:
+#        print("Unknown command")
+#else:
+#    print("Wrong parameters")
