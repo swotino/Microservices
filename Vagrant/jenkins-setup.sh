98a3dcef602a08587055c4178a940da97b1a809b
Leonardo <swotino@hotmail.it>
2022-12-13 17:30:44 +0100

    Fixed the jenkins-setup.sh with Maven v3.8.6

--- Vagrant/jenkins-setup.sh
+++ Vagrant/jenkins-setup.sh
@@ -2,7 +2,14 @@
 
 # Install Java
 sudo apt-get update
-sudo apt-get install -y openjdk-7-jdk
+sudo apt-get install -y openjdk-17-jdk
+
+# Install maven 3.8.6
+wget https://downloads.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
+tar -xvzf apache-maven-3.8.6-bin.tar.gz
+rm apache-maven-3.8.6-bin.tar.gz
+sudo mv apache-maven-3.8.6 /opt/maven
+sudo ln -s /opt/maven/bin/mvn /usr/bin/mvn
 
 # Install Jenkins
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
